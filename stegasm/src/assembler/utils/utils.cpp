4b2fb90d204d04dd1997a5efc8fb3e3f59cb344c
roumite <julien.serin4@gmail.com>
2026-04-05 18:07:52 -0400

    feat(): add parsing of float in stegasm assembler

--- stegasm/src/assembler/utils/utils.cpp
+++ stegasm/src/assembler/utils/utils.cpp
@@ -50,21 +50,60 @@ namespace assembler
         return {};
     }
 
-    bool token_is_uint32_value(const std::string& token)
+    inline bool is_float(const std::string &token)
     {
-        uint32_t result;
-        auto [ptr, ec] = std::from_chars(token.data(), token.data() + token.size(), result);
-        return ec == std::errc();
+        return (token.find('.') != std::string::npos) ||
+            token.ends_with('f') ||
+            token.ends_with('F');
     }
 
-    uint32_t token_to_uint32(const std::string& token)
+    bool token_is_numeric_value(const std::string& token)
     {
-        if (token_is_uint32_value(token))
+        if (token.empty()) return false;
+
+        if (is_float(token))
+        {
+            std::string clean_token = token;
+            if (clean_token.ends_with('f') || clean_token.ends_with('F')) {
+                clean_token.pop_back();
+            }
+
+            float result;
+            auto [ptr, ec] = std::from_chars(clean_token.data(), clean_token.data() + clean_token.size(), result);
+
+            return ec == std::errc() && ptr == clean_token.data() + clean_token.size();
+        }
+        uint32_t result;
+        auto [ptr, ec] = std::from_chars(token.data(), token.data() + token.size(), result);
+        return ec == std::errc() && ptr == token.data() + token.size();
+
+    }
+
+    uint32_t token_to_numeric(const std::string& token)
+    {
+        if (is_float(token))
+        {
+            std::string clean_token = token;
+            if (clean_token.ends_with('f') || clean_token.ends_with('F')) {
+                clean_token.pop_back();
+            }
+
+            float result;
+            auto [ptr, ec] = std::from_chars(clean_token.data(), clean_token.data() + clean_token.size(), result);
+
+            if (ec == std::errc() && ptr == clean_token.data() + clean_token.size()) {
+                return std::bit_cast<uint32_t>(result);
+            }
+        } else
         {
             uint32_t result;
-            std::from_chars(token.data(), token.data() + token.size(), result);
-            return result;
+            auto [ptr, ec] = std::from_chars(token.data(), token.data() + token.size(), result);
+
+            if (ec == std::errc() && ptr == token.data() + token.size()) {
+                return result;
+            }
         }
-        Linter::error("Invalid uint16 value \"" + token + "\"");
+
+        Linter::error("Invalid numeric value \"" + token + "\"");
     }
 }
